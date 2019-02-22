63a33a197b37563702c076eed2ed80e0f3438508
dshirle7 <david@shirleymath.com>
2019-02-22 15:43:08 -0800

    First commit; empty cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='[D[D[D[Dngboard Data Science Career Track. Data obtained from Airbnb's Kaggle competition.',
+    author='David Shirley',
+    license='MIT',
+)
